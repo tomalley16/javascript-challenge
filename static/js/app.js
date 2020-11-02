0fcbcc81a2d52ccac44251d80f1581447b47ea98
Timothy O'Malley <tomalley@landmarkeducation.com>
2020-11-02 13:06:48 -0600

    Updated app.js file through created table rows

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,34 @@
 // from data.js
 var tableData = data;
 
+var tbody = d3.select("tbody");
+
 // YOUR CODE HERE!
+console.log(data);
+
+// 1) Looping through that data and console.log each object:
+data.forEach(function(ufoReport) {
+    console.log(ufoReport);
+});
+
+// 2) Using D3 to append one talbe row for each object:
+// data.forEach(function(ufoReport) {
+//     console.log(ufoReport);
+//     var row = tbody.append("tr");
+
+//     Object.entries(ufoReport).forEach(function([key, value]) {
+//         console.log(key, value)
+//     });
+// });
+
+//  3) Use D3 to append 1 cell per weather report value (date/time, city, state, country, shape, comment:
+data.forEach(function(ufoReport) {
+    console.log(ufoReport);
+    var row = tbody.append("tr");
+
+    Object.entries(ufoReport).forEach(function([key, value]) {
+        console.log(key, value)
+
+        var cell = tbody.append("td");
+    });
+});
